aaca1335543e8b8b8bbc5ae27ef0ea7451ee741f
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2022-01-29 09:46:00 +0530

    completed header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,22 @@
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
+    colors : {
+      transparent : 'transparent',
+      primary : '#52BD95',
+      secondary : '#E5F5EF',
+      tertiary : '#51CBFF',
+      black : {
+        dark : '#1E1E1E',
+        light : '#3F3F3F'
+      },
+      gray : '#5F5B80',
+      white : '#ffffff'
+    },
+    fontFamily: {
+      heading : 'Roboto, sans-serif',
+      body: 'Inter, sans-serif'
+    },
     extend: {},
   },
   plugins: [],
