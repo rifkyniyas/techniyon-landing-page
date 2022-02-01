354b402a02c341c3d0b3e1ca602dad17b5d17850
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2022-02-02 03:07:42 +0530

    fixed the bug in previous commit

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ console.log("SCRIPT LOADED SUCCESSFULLY")
 const faqButtons = document.getElementsByClassName("faq_button");
 
 for (let i = 0; i < faqButtons.length; i++) {
-  faqButtons[i].addEventListener("click", (this) => {
-    this.classList.toggle("active");
-    const faqAnswer = this.nextElementSibling;
+  faqButtons[i].addEventListener("click", (event) => {
+    event.target.classList.toggle("active");
+    const faqAnswer = event.target.nextElementSibling;
     if (faqAnswer.style.display === "block") return faqAnswer.style.display = "none";
     faqAnswer.style.display = "block";
   });
