f6a8a2a40c3ebf5964e2057a9134256fe8a08d38
Rifky Niyas <63934498+MohammadRifky@users.noreply.github.com>
2022-02-02 16:15:49 +0530

    added interactivity

--- main.js
+++ main.js
@@ -4,9 +4,16 @@ const faqButtons = document.getElementsByClassName("faq_button");
 
 for (let i = 0; i < faqButtons.length; i++) {
   faqButtons[i].addEventListener("click", (event) => {
-    event.target.classList.toggle("active");
+    const plusIcon = event.target.children[0]
+    const minusIcon = event.target.children[1]
     const faqAnswer = event.target.nextElementSibling;
-    if (faqAnswer.style.display === "block") return faqAnswer.style.display = "none";
-    faqAnswer.style.display = "block";
+    if (faqAnswer.style.display === "block") {
+      plusIcon.style.display = "block"
+      minusIcon.style.display = "none"
+      return faqAnswer.style.display = "none"
+    }
+    plusIcon.style.display = "none"
+    minusIcon.style.display = "block"
+    faqAnswer.style.display = "block"
   });
 }
